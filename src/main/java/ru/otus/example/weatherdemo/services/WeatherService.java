68f54173ae5331a5f5203169caea6d9da7473e15
ydvorzhetskiy <dvorzhetskij@mail.ru>
2019-12-12 21:58:17 +0600

    -4

--- src/main/java/ru/otus/example/weatherdemo/services/WeatherService.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.example.weatherdemo.services;
-
-import ru.otus.example.weatherdemo.model.Weather;
-
-import java.util.List;
-
-public interface WeatherService {
-    List<Weather> gWeather();
-}
